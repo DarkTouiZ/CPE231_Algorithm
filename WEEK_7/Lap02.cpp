75d84255cadc5717cc1c4a9fdbf28c8461268931
Adisorn Parama <adisorn.safe01@gmail.com>
2024-10-02 11:34:34 +0700

    Update Lap02.cpp

--- WEEK_7/Lap02.cpp
+++ WEEK_7/Lap02.cpp
@@ -3,8 +3,14 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
+bool compare(int a, int b)
+{
+    return a > b;
+}
+
 
 int main()
 {
@@ -15,34 +21,40 @@ int main()
         cin >> arr[i];
     }
 
-    vector<int> sortedList(n);
-    //MaxHeapBottomUp
-    for(int i = n/2 - 1; i >= 1; --i){
+    //MinHeapSort
+    for(int i = n / 2 - 1; i >= 0; --i){
         int k = i;
-        int v = arr[i];
+        int v = arr[k];
         bool heap = false;
-        while (!heap && 2*k <= n){
-            int j = 2*k;
-            if(j < n){
-                if(arr[j] < arr[j+1])
-                    j = j + 1;
+        //heapify 
+        while(!heap && 2 * k + 1 < n){
+            int j = 2 * k + 1; // why 2 * k + 1? because we need to find the left child of the node
+            if(j < n - 1){ // if there is a right child
+                if(arr[j] > arr[j + 1]){ // if right child is smaller than left child
+                    j += 1;
+                }
             }
 
-            if(v >= arr[j]){
+            if(v <= arr[j]){ // if the parent is smaller than the child
                 heap = true;
-            }else{
-                arr[k] = arr[j];
-                k = j;
+            }
+            else{
+                arr[k] = arr[j]; // swap the parent and the child
+                k = j; // move to the child
             }
         }
-        arr[k] = v;
+        arr[k] = v; // put the parent to the right place
     }
 
-    //print result
-    for(auto it : arr)
-        cout << it << " "
+    //print MinHeapSort
+    for(int i = 0; i < n; ++i){
+        cout << arr[i] << " ";
+    }
     cout << endl;
 
-    
-    return 0;
+    //print sorted array
+    sort(arr.begin(), arr.end(), compare);
+    for(int i = 0; i < n; ++i){
+        cout << arr[i] << " ";
+    }
 }
